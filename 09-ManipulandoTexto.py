bde658839cd7f42cb2d05cab3a87c8c14ccf35d8
Gabriel Koch <gabrielkochsilva@gmail.com>
2026-04-20 17:20:54 -0300

    Refactor string manipulation examples in Python

--- 09-ManipulandoTexto.py
+++ 09-ManipulandoTexto.py
@@ -1,69 +1,68 @@
+#STRINGS EM PYTHON SÃO IMUTAVEIS;
+#NENHUM METODO ALTERA DIRETAMENTE;
+#ELES SEMPRE RETORNAM ALGO EM UMA NOVA VARIAVEL
+
 frase = 'Curso em Vídeo Python'
-#        012345678901234567890   
-print(frase[9])
-#imprime apenas o nono indice ('V')
-print(frase[9:14])
-#imprime apenas os caracteres do indice 9 até o 13 (Vídeo)
-print(frase[9:21:2])
-#imprime a frase (Vídeo Python), porém, ignorando um caractere (ou pulando de dois em dois) (VdoPto)
-print(frase[:5])
-#Vai do indice 0 até o N-1
-print(frase[9:])
-#Incia no indice 15 (P) e vai até o final da string
-print(frase[9::3])
-#Começa no 9, vai até o final ignorando dois caracteres seguidos (pula de tres em tres)(Vphe)
-#[inicio:fim:ignora caracteres]
-#ANALISE
+len(frase) # = 21 caracteres
 
-print('Video' in frase)
-#TRUE or FALSE
+print(frase[9]) # = 'V'
+print(frase[9:13]) # [9:N-1] 'Víde'
+print(frase[9:21:2]) #'VdoPto'
 
-len(frase) #lengh = conta a quantidade de caracteres
+'''Você pode omitir o indice inicial 
+ou final 
+[:12] = escreve do começo até o 11.
+[15:] = escreve da posição inicial até o fim
+da string'''
 
-frase.count('o', 0,13) #o programa conta quantas vezes aparece a letra 'o' minuscula na frase = 3
-#contando do indice 0 e indo até 13-1
+frase.count('o',0,13)
+'''Conta quantas vezes aparece a letra "o" 
+da posição 0 a 12'''
 
 frase.find('deo')
-#isso vai retornar a POSIÇÃO 11, pois esse é o indice onde inicia a sequencia 'deo'
-#caso retorne -1 significa que o find não achou a POSIÇÃO na string
+''' = 11; vai retornar a posição onde se
+inicia a frase "deo"; caso não encontre ele 
+retorna -1 '''
 
-frese = ('Sou usuário de Iphone')
-android = frese.replace('Iphone','Android')
-print(android)
-#replace é usado para substitui a palavra escolhida (Iphone) pela palavra desejada (Android)
+print('Curso' in frase)
+# true | false
 
-frase.upper() , frase.lower() #MAIUSCULA e minuscula
+exemplo = 'Adoro mexer em Javascript'
+print(exemplo)
+exemplo.replace('Javascript','Python')
+print(exemplo)
 
-frase.capitalize #A string inteira fica em minuscula, exceto a primeira letra de toda a string
+nova_frase = '   Aprenda Python  '
+print(nova_frase.strip())# remove espaços indesejados
+''' .rstrip() e lstrip() = right e left strip '''
 
-frase.title #Identifica palavras separadas e as primeiras letras de cada palavra ficam MAIUSCULA
+teste1 = 'Curso em Vídeo Python'
+teste_separado = teste1.split()
+# [Curso] [em] [Vídeo] [Python]
+# [01234] [01] [01234] [012345]
+#    0      1     2       3
+print(teste_separado)
 
-frase.strip #remove espaços inuteis antes da frase e no final da frase '   OI   '
+teste1 = '-'.join(teste1)
+print(teste1)
 
-frase.rstrip() #remove apenas os espaços no lado direito
-frase.lstrip() #remove apenas os espaços no lado esquerto
+china = 'chines safado maluco   '
+print(len(china)) #frase c/ os espaços no meio e fim
+print(len(china.strip()))# frase s/ espaço no fim
+print(len(china.replace(' ',''))) #frase sem espaços no meio
+# se for para remover TODOS os espaços, o replace 
+# faz o papel do strip
+# replace já reajusta a frase automaticamente
 
-#DIVISÃO DE STRING
+print(china.find('nes')) # = 3
 
-frase = 'Curso em Vídeo Python'
-frase.split() #cada palavra é separa e recebe indice proprio, e cada mini-repartição tbm recebe indice
-#'Curso em Video Python'
-# 01234 01 01234 012345
-#  1     2   3     4 
+new_china = china.split()
+print(new_china[2][4])
+#        2 = maluco 4 = 'c' 
 
-divido = frase.split()
-print(divido[0][3])#O 0 mostra apenas o primeiro conjunto, e o 3 a terceira letra
-
-print('O rato \nroeu a roupa \ndo rei de Roma') #o \n serve para quebrar linhas
-
-print('Quer fazer assim: você tenta responder as 20 questões?', end=' ')
-#end=' ' serve para dar espaço e continuar o print de baixo
-print('Posso corrigir por partes se preferir (tipo 1–10 e depois 11–20). Como prefere?')
-
-nome = input('digite seu nome ')
-print('prazer {:20}!'.format(nome))
-print('prazer {:>20}!'.format(nome))
-print('prazer {:^20}!'.format(nome))
-#O valor de nome ocupou 20 espaços de texto: GABRIEL       !
-#Usando {:>20} o nome ocupa 20 espaços, porem está alinhado a direita:        GABRIEL!
-#Utilizando {:^20} o nome ocupa um total de 20 espaços, porem centralizando o nome:      GABRIEL    .
+num = int(input('escreva um numero de 1 a 9999 '))
+u = num // 1 % 10
+d = num // 10 % 10
+c = num // 100 % 10 
+m = num // 1000 % 10
+print(f'unidade = {u}\n dezena = {d}\n centena = {c}\n milhar = {m}')
