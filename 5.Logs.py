d0bcbec0bc3a7ad964e0328bda007fbe82dc43d4
Gabriel Koch <gabrielkochsilva@gmail.com>
2026-04-24 17:05:13 -0300

    Update 5.Logs.py

--- 5.Logs.py
+++ 5.Logs.py
@@ -1,45 +1,105 @@
 # =====================================================================
-# 📋 Você recebe uma lista de registros de log de um servidor. 
-# Cada linha tem o formato 'TIPO - mensagem'. 
-# Crie uma função que conta quantos logs são de cada tipo e lista as mensagens de erro.
+# Dada uma lista de números: Retorne quantos são maiores que a média
+# =====================================================================
+lista = []
+tam = int(input('Digite o Tamanho da lista: '))
+for i in range(tam):
+    lista.append(int(input(f'digite o {i}º valor: ')))
+
+sum = 0
+for i in range(len(lista)):
+    sum += lista[i]
+media = sum / tam
+
+print(f'A soma de todos os valores são {sum} e a média {media}')
+cont = 0
+
+for i in range(tam):
+    if lista[i] > media:
+        print(f'Valor acima da média: {lista[i]}')
+        cont += 1
+print(f'Existem {cont} Valores acima da média')
+
+# =====================================================================
+# Leia 10 números em uma lista e mostre: Vetor original & Vetor invertido (Sem Métodos)
 # =====================================================================
 
-def typelogs(logs):
-    cInfo = 0
-    cErro = 0
-    cAviso = 0
-    logs_tratados = []
-    erros = []
+num = []
+for i in range(4):# = 0,1,2,3 = 4 espaços
+    valor = int(input('Digite 4 Números: '))
+    num.append(valor)
 
-    for i in range(len(logs)):
-        logs_tratados.append(logs[i].split(' - '))  # acumula, não substitui
-      # logs_tratados = [['INFO','xxxxxx']['AVISO','yyyyyyyy'][...]]
-    for j in range(len(logs_tratados)):
-        tipo = logs_tratados[j][0]      # 'INFO', 'ERRO' ou 'AVISO'
-        mensagem = logs_tratados[j][1]  # texto depois do ' - '
+num_inv =[0,0,0,0]
+c= 0
+for i in range(3,-1,-1): # num = [{0}{1}{2}{3]}; -1 porque se fosse 0 ele iria apenas até o 1 (inicio,fim-1)
+    num_inv[c] = num[i]
+    c += 1
 
-        if tipo == 'INFO':
-            cInfo += 1
-        elif tipo == 'AVISO':
-            cAviso += 1
-        else:
-            cErro += 1
-            erros.append(mensagem)
+print(num)
+print(num_inv)
 
-    print(f'INFO: {cInfo}')
-    print(f'AVISO: {cAviso}')
-    print(f'ERRO: {cErro}')
-    print('\nMensagens de erro:')
-    for erro in erros:
-        print(f'- {erro}')
+# =====================================================================
+# Dada a lista: lista2 = [1,2,3,4,5]: Peça um valor e remova todas as ocorrências dele manualmente (sem usar remove())
+# =====================================================================
 
-logs = [
-  "INFO - Servidor iniciado",
-  "INFO - Usuário conectado",
-  "ERRO - Falha na conexão",
-  "INFO - Arquivo salvo",
-  "ERRO - Timeout na requisição",
-  "AVISO - Memória acima de 80%"
-]
+lista2 = [1,2,3,4,5]
 
-typelogs(logs)
+escolhido = int(input('Escolha um numero a ser removido: '))
+for i in range(len(lista2)):
+    if escolhido == lista2[i]:
+        for j in range(i,len(lista2)- 1):
+            lista2[j] = lista2[j+1]
+        lista2.pop()#remove o último valor da lista, evitando que haja valor duplicado
+        break
+
+print(f'{escolhido}REMOVIDO!')
+print(lista2)
+
+# =====================================================================
+# Peça um número ao usuário e: Retorne a posição dele ou '-1' se não existir
+# =====================================================================
+
+lista1 = [5,8,2,9,1]
+achou = 0
+posicao = 0
+
+numero = int(input('digite um numero, vamos ver se ele está na lista!: '))
+
+for i in range(len(lista1)):
+    if numero == lista1[i]:
+        posicao = i
+        print(f'O valor está na posição {posicao}')
+
+if numero not in lista1:
+    achou -= 1
+    print(achou)
+
+# =====================================================================
+# PLeia uma lista de números inteiros informada pelo usuário. 
+# Em seguida, crie uma segunda lista contendo apenas os valores únicos da lista original, sem repetição.
+# Ao final, ordene essa nova lista em ordem crescente e exiba o resultado.
+# =====================================================================
+
+lista = []
+nova = []
+tam = int(input('Digite o tamanho da lista: '))
+
+for i in range(tam):
+    lista.append(int(input(f'Digite o {i+1}º valor: ')))
+
+def nova_lista(listar, novo):
+    for i in range(len(listar)):
+        if listar[i] not in novo:
+            novo.append(listar[i])
+
+    for k in range(len(novo)):  # bubble sort
+        for l in range(len(novo) - 1):
+            if novo[l] > novo[l + 1]:
+                aux = novo[l]
+                novo[l] = novo[l + 1]
+                novo[l + 1] = aux
+
+    return novo
+
+nova_lista(lista, nova)
+print(nova)
