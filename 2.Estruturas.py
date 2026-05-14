79cbfefa1ed6f09f26289355cad264ee378464c4
Gabriel Koch <gabrielkochsilva@gmail.com>
2026-05-14 15:59:13 -0300

    Update 2.Estruturas.py

--- 2.Estruturas.py
+++ 2.Estruturas.py
@@ -1,19 +1,25 @@
 # ======================================================================
 # LISTAS
-# ======================================================================
+# Use lista quando:
 
-# Indexáveis
-# Ordenadas
-# Permite diferentes tipos
-# Mutáveis
+# - Você tem uma coleção ordenada de itens,
+# - Geralmente do mesmo tipo, que:
+# - Pode crescer, diminuir ou mudar.
+
+# - Permite vários tipo;
+# - mantém ordem;
+# - permite repetição;
+# - permite alteração, mutável;
+# - acessa por índice;
+# ======================================================================
 
 numeros = [10, 20, 30]
 nomes = ["Ana", "Carlos", "João"]
 misturado = [1, "A", True, [12,"3"]]
 
-# ==========
+# ======================================================================
 # MÉTODOS:
-# ==========
+# ======================================================================
 
 nomes.append('Gabriel')  # Adiciona "Gabriel" ao fim da lista
 nomes.insert(1,"Luiz")   # Adiciona "Luiz" na posição [1], os elementos a direita são movidos [i+1] 
@@ -25,6 +31,119 @@ nomes.sort(reverse=True) # Organiza a lista em ordem decrescente
 nomes.reverse()          # Inverte a ordem atual da lista. [3,2,6] -> [6,2,3]
 nomes.count("Ana")       # Conta quantas vezes o nome Ana aparece na lista
 nomes.index("Carlos")    # Retorna o indice do valor fornecido
+
+# ======================================================================
+# TUPLA
+# Use tupla quando:
+# Você tem um grupo fixo de valores que pertencem juntos e cuja posição tem significado:
+
+jogada = ("Alice", 30) 
+coordenada = (10, 25)
+
+# - mantém ordem
+# - permite repetição
+# - é imutável
+# - boa para registros pequenos e fixos
+# - Indexáveis
+# - Úteis para dados fixos
+# ======================================================================
+
+numeros = (10, 20, 30)
+nomes = ("Ana", "Carlos", "João")
+misturado = (1, "A", True, (1,3))
+
+# Permite desempacotamento, assim como listas, desde que 
+# o numero de variaveis corresponda ao numero de indices da tupla
+tuplinha = (1,2,3,4,5)
+
+a,b,c,d,e = tuplinha 
+(x,y,*z) = tuplinha 
+x = a
+y = b
+z = [c,d,e]
+# O * torna a variavel em lista, 
+# permitindo que os valores faltantes sejam atribuidos e ninguem fique de fora
+
+# A unica forma de incluir e remover valores de uma tupla é convertendo ela em uma lista e depois para tupla novamente
+thistuple = ("apple", "banana", "cherry")
+k = list(thistuple)
+k.append("orange")
+thistuple = tuple(k)
+
+# Tuplas podem ser Concatenadas
+tuple1 = ("a", "b" , "c")
+tuple2 = (1, 2, 3)
+
+tuple3 = tuple1 + tuple2
+print(tuple3)
+
+# ======================================================================
+# SETS
+# Use set quando você quer valores únicos e não se importa com a ordem.
+# - não mantém ordem confiável
+# - não permite repetição
+# - busca muito rápida
+# - ótimo para remover duplicados
+# - verificar existência rapidamente
+# - comparar conjuntos
+# - encontrar interseção/diferença
+# ======================================================================
+
+# Não possuem índice
+# Não garantem ordem
+# Não aceitam duplicados
+# Ótimos para operações de conjunto
+# Tipos mistos
+
+# Remover duplicados
+# Comparar listas
+# Descobrir itens em comum
+
+s = {1, 2, 3}
+s.add(7) # Adiciona um valor
+s.update([1,2,3]) # Adiciona VÁRIOS valores
+s.remove(10) # Caso não exista, dá erro
+s.discard(10) # Caso não exista, não dá erro
+s.clear() # Esvazia o set
+
+a = {1,3,5,2,6}
+b = {3,2,5,6,8,10}
+
+a.union(b) # A + B, sem repetição
+a.intersection(b) # A = B, existe em ambos
+a.difference(b) # A - B, exclusivo de A
+a.symmetric_difference(b) # (A - B) + (B - A), exclusivo de cada um
+
+# ======================================================================
+# DICT
+# - chave única
+# - valor pode ser qualquer coisa
+# - busca rápida por chave
+# - ideal para cadastros, contadores, agrupamentos e registros
+# - key:value
+# ======================================================================
+
+# Dentro de cada dicionário a chave deve ser unica
+
+d = {
+    "nome": "Gabriel",
+    "idade": 25,
+    "cidade": "SC"
+}
+
+# Acesso:
+print(d["nome"]) # = Gabriel
+
+d.get("nome") # Pegar valor
+d["idade"] = 30 # Atribuir/Substituir
+d.keys()   # lista chaves
+d.values() # lista valores
+d.items()  # lista chaves + valore
+d.pop("idade") # Remove valor
+d.update({"cidade": "RJ"}) # Adicionar/criar key:value
+d["sexo"] = "Masculino" # Pode-se criar e adicionar diretamente, sem função
+
+
 
 # =================================================================================
 # LIST COMPREHENSION
@@ -64,34 +183,6 @@ print(pares)
 # resultado recebe os valores atuais de numeros, vezes 2, caso o valor atual seja menor 10 e for par
 resultado = [x * 2 for x in numeros if x < 10 if x % 2 == 0]
 
-# ======================================================================
-# SETS
-# ======================================================================
-
-# Não possuem índice
-# Não garantem ordem
-# Não aceitam duplicados
-# Ótimos para operações de conjunto
-# Tipos mistos
-
-# Remover duplicados
-# Comparar listas
-# Descobrir itens em comum
-
-s = {1, 2, 3}
-s.add(7) # Adiciona um valor
-s.update([1,2,3]) # Adiciona VÁRIOS valores
-s.remove(10) # Caso não exista, dá erro
-s.discard(10) # Caso não exista, não dá erro
-s.clear() # Esvazia o set
-
-a = {1,3,5,2,6}
-b = {3,2,5,6,8,10}
-
-a.union(b) # A + B, sem repetição
-a.intersection(b) # A = B, existe em ambos
-a.difference(b) # A - B, exclusivo de A
-a.symmetric_difference(b) # (A - B) + (B - A), exclusivo de cada um
 
 # ==========================================================================================================================
 # SET COMPREHENSION
@@ -130,88 +221,6 @@ print(emails_normalizados)
 palavras = ["python", "sql", "dados", "ti", "engenharia"]
 
 palavras_grandes = {palavra for palavra in palavras if len(palavra) > 3}
-
-
-# ======================================================================
-# TUPLA
-# ======================================================================
-
-# Ordenadas
-# Indexáveis
-# Aceitam repetição
-# Imutáveis
-# Úteis para dados fixos
-
-numeros = (10, 20, 30)
-nomes = ("Ana", "Carlos", "João")
-misturado = (1, "A", True, (1,3))
-
-# ==========
-# MÉTODOS: IGUAIS OS DA LISTA
-# ==========
-
-# Assim como a lista, pode ser acessada a ultima posição com [-1]
-# [2:5] acessa tupla na posição 2 (incluida) até a 5-1
-
-#Permite desempacotamento desde que o numero de variaveis corresponda ao numero de indices da tupla
-tuplinha = (1,2,3,4,5)
-
-a,b,c,d,e = tuplinha 
-(x,y,*z) = tuplinha # O * Torna a variavel em lista, permitindo que os valores faltantes sejam atribuidos e ninguem fique de fora
-
-
-# A unica forma de incluir e remover valores de uma tupla é convertendo ela em uma lista e depois para tupla novamente
-thistuple = ("apple", "banana", "cherry")
-k = list(thistuple)
-k.append("orange")
-thistuple = tuple(k)
-
-# Tuplas podem ser Concatenadas
-tuple1 = ("a", "b" , "c")
-tuple2 = (1, 2, 3)
-
-tuple3 = tuple1 + tuple2
-print(tuple3)
-
-
-# ======================================================================
-# DICT
-# ======================================================================
-
-# Chave única
-# Valor pode repetir
-# Mutável
-# Muito rápido para busca por chave
-
-# USADO EM:
-# JSON
-# APIs
-# cadastro
-# configurações
-# contadores
-# registros
-
-
-# "key":value
-# Dentro de cada dicionário a chave deve ser unica
-
-d = {
-    "nome": "Gabriel",
-    "idade": 25,
-    "cidade": "SC"
-}
-
-# Acesso:
-print(d["nome"]) # = Gabriel
-
-d.get("nome") # Pegar valor
-d["idade"] = 30 # Atribuir/Substituir
-d.keys()   # lista chaves
-d.values() # lista valores
-d.items()  # lista chaves + valore
-d.pop("idade") # Remove valor
-d.update({"cidade": "RJ"}) # Adicionar/criar key:value
-d["sexo"] = "Masculino" # Pode-se criar e adicionar diretamente, sem função
 
 # ==========================================================================================================================
 # DICT COMPREHENSION
