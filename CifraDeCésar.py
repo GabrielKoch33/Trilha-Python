e31e8b3a928435f932694fe3dacd125367223464
Gabriel Koch <gabrielkochsilva@gmail.com>
2026-05-19 08:53:43 -0300

    Update CifraDeCésar.py

--- CifraDeCésar.py
+++ CifraDeCésar.py
@@ -1,20 +1,22 @@
-
 alfabeto = list("abcdefghijklmnopqrstuvwxyz1234567890") 
-palavra = list(input('Digite uma palavra: ').strip().replace(' ','-').lower()) #antes de transformar a frase em lista, eu tiro os espaços e substituo-os por '-'
+
+palavra = list(input('Digite uma palavra: ').strip().replace(' ','-').lower()) 
+#antes de transformar a frase em lista, eu tiro os espaços e substituo-os por '-'
 # assim toda frase vai ser: 'hoje-e-um-belo-dia' ao invés de = [[hoje],[e],[um],[belo],[dia]]
+
 chave = int(input('digite o valor que será deslocado: ')) # fazer amanhã <--
 
-for indice, letra in enumerate(palavra): # percorre toda a palavra que foi informada, de letra em letra
+for indice, letra in enumerate(palavra): # percorre cada letra da palavra informada
     for index, caractere in enumerate(alfabeto): # percorre todo o alfabeto
         if letra == '-': 
             break
-        if letra == 'z': # se a letra da palavra for z, então recebe 'a'
+        elif letra == 'z': # se a letra da palavra for z, então recebe 'a'
             palavra[indice] = 'a'
             break
-        if letra == '0':
+        elif letra == '0':
             palavra[indice] = '1'
             break
-        elif letra == caractere: # se não for z, vai percorrer o alfabeto ate achar a letra igual
+        elif letra == caractere: # se for qualquer outra letra ou número, vai percorrer o alfabeto ate achar a letra igual
             palavra[indice] = alfabeto[index+1]
             break
     
